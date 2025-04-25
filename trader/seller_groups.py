456466221026fc5f5839600f86cbe980196ceea0
silver6wings <silver6wings@163.com>
2025-04-25 09:44:45 +0800

    Update seller_groups.py

--- trader/seller_groups.py
+++ trader/seller_groups.py
@@ -11,16 +11,22 @@ class GroupSellers:
                 parent.__init__(self, strategy_name, delegate, parameters)
         print('>> 初始化完成')
 
-    def group_check_sell(self, code: str, quote: Dict, curr_date: str, curr_time: str, position: XtPosition,
-                         held_day: int, max_price: Optional[float], history: Optional[pd.DataFrame]) -> bool:
+    def group_check_sell(
+            self, code: str, quote: Dict, curr_date: str, curr_time: str,
+            position: XtPosition, held_day: int, max_price: Optional[float],
+            history: Optional[pd.DataFrame], ticks: Optional[list[list]], extra: any,
+    ) -> bool:
         sold = False
         for parent in self.__class__.__bases__:
             if parent.__name__ != 'GroupSellers':
                 if sold:
                     break
                 else:
-                    sold = parent.check_sell(self, code=code, quote=quote, curr_date=curr_date, curr_time=curr_time,
-                                             position=position, held_day=held_day, max_price=max_price, history=history)
+                    sold = parent.check_sell(
+                        self, code=code, quote=quote, curr_date=curr_date, curr_time=curr_time,
+                        position=position, held_day=held_day, max_price=max_price,
+                        history=history, ticks=ticks, extra=extra,
+                    )
         return sold
 
 
@@ -30,8 +36,8 @@ class ClassicGroupSeller(GroupSellers, HardSeller, FallSeller, ReturnSeller):
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 均线卖出
@@ -40,8 +46,8 @@ class ClassicMAGroupSeller(GroupSellers, HardSeller, FallSeller, ReturnSeller, M
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 监控卖出
@@ -50,8 +56,8 @@ class ShieldGroupSeller(GroupSellers, HardSeller, FallSeller, ReturnSeller):
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # Deepseek
@@ -60,8 +66,8 @@ class DeepseekGroupSeller(GroupSellers, HardSeller, SwitchSeller, FallSeller):
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 龙抬头
@@ -70,8 +76,8 @@ class LTT2GroupSeller(GroupSellers, HardSeller, OpenDaySeller, SwitchSeller, Ret
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 三倍量突破
@@ -80,8 +86,8 @@ class T3BLGroupSeller(GroupSellers, HardSeller, SwitchSeller, FallSeller, MASell
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 平台绿缩
@@ -90,8 +96,8 @@ class PTLSGroupSeller(GroupSellers, HardSeller, UppingBlocker, FallSeller, Retur
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
 
 
 # 金雀突破
@@ -100,5 +106,5 @@ class JQTPGroupSeller(GroupSellers, FallSeller, DropSeller, HardSeller, WRSeller
         super().__init__()
         self.group_init(strategy_name, delegate, parameters)
 
-    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history):
-        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history)
+    def check_sell(self, code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra):
+        self.group_check_sell(code, quote, curr_date, curr_time, position, held_day, max_price, history, ticks, extra)
